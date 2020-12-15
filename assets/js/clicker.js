c311a5f9a72857a8bfc9b375312809e7033d8e67
Mykhail23509 <71732087+Mykhail23509@users.noreply.github.com>
2020-12-16 01:24:06 +0200

    Clicker v1.5.5

--- assets/js/clicker.js
+++ assets/js/clicker.js
@@ -1,4 +1,5 @@
 // DATA BASE
+
 const appDataBase = {
     numbeOfClicks: 0
 }
@@ -9,12 +10,4 @@ const clickerClick = document.querySelector('.score-point');
 function onButtonClick1() {
     appDataBase.numbeOfClicks += 1;
     clickerClick.innerHTML = appDataBase.numbeOfClicks;
-}
-document.querySelector('.test').appendChild(clickerClick);
-
-// Achieve
-function AchieveFirst(){
-    if(appDataBase.numbeOfClicks >= 10) {
-        alert('You master!');
-    }
 }
