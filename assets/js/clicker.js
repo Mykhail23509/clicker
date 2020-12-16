d275b4eebd54b00b2c9f6bfc9429fe5aae2257bc
Mykhail23509 <71732087+Mykhail23509@users.noreply.github.com>
2020-12-16 23:36:19 +0200

    Clicker v2.0
    - Added Shop

--- assets/js/clicker.js
+++ assets/js/clicker.js
@@ -11,3 +11,30 @@ function onButtonClick1() {
     appDataBase.numbeOfClicks += 1;
     clickerClick.innerHTML = appDataBase.numbeOfClicks;
 }
+
+function onButtonClick2() {
+    appDataBase.numbeOfClicks += 2;
+    clickerClick.innerHTML = appDataBase.numbeOfClicks;
+}
+
+function onButtonClick0() {
+    appDataBase.numbeOfClicks += 0;
+    clickerClick.innerHTML = appDataBase.numbeOfClicks;
+}
+
+// shop
+const buttonUpgrader = document.querySelector('.button-clicker');
+function buyClickUpgrade() {
+    let ClickUpgrade = document.querySelector('.shopClickUpgrade');
+    if(appDataBase.numbeOfClicks >= 10){
+        alert('You bought it!');
+        appDataBase.numbeOfClicks -= 10;
+        onButtonClick0()
+        buttonUpgrader.setAttribute( 'onclick', 'onButtonClick2()' );
+        ClickUpgrade.innerHTML = 'Max Level 1/1';
+        ClickUpgrade.removeAttribute( 'onclick', 'buyClickUpgrade()' );
+
+    } else {
+        alert('You dont have enough money');
+    }
+}
